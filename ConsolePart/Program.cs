e96b99efa97f471728853edf6c5b93a0d0c68d0f
svi33 <swigor14@gmail.com>
2020-05-18 11:50:32 +0300

    settings

---
+++ ConsolePart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
